186536144744b820ef0b8364bb7a667e260375ba
Francesco <craighero.francesco@gmail.com>
2019-04-05 11:34:00 +0200

    Rebuilt project with electron-forge. Added Semantic-UI. Added button to select CSV file.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// script for index.html
+const {ipcRenderer}=require('electron')
+
+function file_sys_toggle(){
+  ipcRenderer.send('click-button','true')
+}
+
+ipcRenderer.send('openFile',()=>{})
+ipcRenderer.on('fileData',(event,data)=>{
+    console.log(data)
+  document.write(data)
+})
